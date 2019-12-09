1756baf2620b625148451a1c4f0b1177aefb43a6
Ziyan Wang <ziyan.wang@ni.com>
2019-12-09 10:53:36 +0800

    quick sort

---
+++ sort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+void swap(int &x, int &y)
+{
+    int t = x;
+    x = y;
+    y = t;
+}
+void sort(int *list, int start, int end)
+{
+    if (start >= end)
+        return;
+    int p = list[start], left = start, right = end;
+    while (left < right)
+    {
+        while (list[right] > p && left < right)
+            --right;
+        list[left] = list[right];
+        while (list[left] <= p && left < right)
+            ++left;
+        list[right] = list[left];
+    }
+    list[left] = p;
+    sort(list, start, left - 1);
+    sort(list, left + 1, end);
+}
+void sort(int *list, int length)
+{
+    sort(list, 0, length - 1);
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int *list = new int[n];
+    for (int i = 0; i < n; ++i)
+        cin >> list[i];
+    sort(list, n);
+    for (int i = 0; i < n; ++i)
+        cout << list[i] << " ";
+    return 0;
+}
